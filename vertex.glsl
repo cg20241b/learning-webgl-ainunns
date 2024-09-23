6a56cf00ba5c1a8fe3f816b309b0eefed14e4fe9
Ainun Nadhifah Syamsiyah <58164571+ainunns@users.noreply.github.com>
2024-09-23 04:24:39 +0000

    fix: normalize the gradient color

--- vertex.glsl
+++ vertex.glsl
@@ -1,7 +1,11 @@
 attribute vec4 aVertexPosition;
 uniform mat4 uTranslationMatrix;
 varying vec2 posColor;
+
 void main(void) {
-    posColor = aVertexPosition.xy;
+    // -0.5 to 0.5
+    // 0.0 to 1.0
+
+    posColor = aVertexPosition.xy + 0.5;
     gl_Position = uTranslationMatrix * aVertexPosition;
 }
